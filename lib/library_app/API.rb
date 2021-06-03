7c788514e740b564a05bd1d7869d1ae84bcf28b8
Luca <lucakanyo@protonmail.com>
2021-06-03 17:40:50 -0400

    added class methods to book class

--- lib/library_app/API.rb
+++ lib/library_app/API.rb
@@ -1,13 +1,23 @@
+require 'pry'
+
 class API 
 
     URL="http://localhost:3000/"
 
-    def self.get_data 
+    def self.get_book_data 
         response = RestClient.get(URL + "books")
         book_data = JSON.parse(response)
-        book_data.map do | hash | 
-            Book.new(hash)
+        book_data.map do |hash| 
+            id = hash['id']
+            title = hash['title']
+            description = hash['description']
+            author = hash['author']
+            read = hash['read']
+            planned = hash['planned']
+            page_count = hash['page_count']
+            Book.new(id, author, title, description, read, planned, page_count)
         end 
+
     end 
 
 end 
--- lib/library_app/Book.rb
+++ lib/library_app/Book.rb
@@ -1,15 +1,41 @@
 class Book 
 
-    attr_accessor :id, :created_at, :updated_at, :author, :title, :description, :read, :planned, :page_count
+    attr_accessor :id,  :author, :title, :description, :read, :planned, :page_count
 
     @@all = [] 
 
-    def initialize(attributes)
-        attributes.each {|key, value| self.send(("#{key}="), value)}
+    def initialize(id, author, title, description, read, planned, page_count)
+        self.id = id
+        self.author =author
+        self.title = title
+        self.description = description
+        self.read = read
+        self.planned = planned
+        self.page_count = page_count
         @@all << self 
     end 
 
     def self.all 
         @@all 
+    end 
+
+    def self.list_books_read
+        self.all.each do | book | 
+            if book.read
+                puts "#{book.author} - #{book.title}"
+                puts book.description
+                puts "Number of pages: #{book.page_count}"
+            end 
+        end 
+    end 
+
+    def self.list_books_planned
+        self.all.each do | book | 
+            if book.planned
+                puts "#{book.author} - #{book.title}"
+                puts book.description != "" ? book.description : "No description yet"
+                puts "Number of pages: #{book.page_count}"
+            end 
+        end 
     end 
 end 
